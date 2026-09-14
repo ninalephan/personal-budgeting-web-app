6703434fa5bfc9865a42b1e8a092f68e93d83041
ninvphan <nlp61@miami.edu>
2026-09-14 09:39:55 -0400

    protected routes

--- src/proxy.ts
+++ src/proxy.ts
@@ -1,10 +1,18 @@
-import { clerkMiddleware } from "@clerk/nextjs/server";
+import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
-const clerkProxy = clerkMiddleware();
+// Everything not listed here requires a signed-in user. /getting-started
+// and /dashboard (and anything else you add later) fall through to auth.protect().
+const isPublicRoute = createRouteMatcher([
+  "/",
+  "/sign-in(.*)",
+  "/sign-up(.*)",
+]);
 
-export function proxy(...args: Parameters<typeof clerkProxy>) {
-  return clerkProxy(...args);
-}
+export default clerkMiddleware(async (auth, req) => {
+  if (!isPublicRoute(req)) {
+    await auth.protect();
+  }
+});
 
 export const config = {
   matcher: [
